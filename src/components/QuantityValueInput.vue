3ef5f0a717b24019507ba3d16932e58ba0ed2a10
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-03-16 11:47:41 +0100

    Create QuantityValueInput component in QueryBuilder
    - Update version of wikit
    - Add i18n entries for quantity input placeholders and labels
    - Add QuantityValueInput component still with duplicated logic from EntityLookup
    
    Code duplication with Lookup component will be addressed in a different patch
    
    Bug: T276943
    Change-Id: I18adab5139223175559400a2d4909c447be03d82

---
+++ src/components/QuantityValueInput.vue
@@ -0,0 +1,142 @@
+<template>
+	<QuantityInput
+		:label="$i18n('query-builder-quantity-value-label')"
+		@update:numberInputValue="$emit( 'numberInputValue', numberInputValue )"
+		@update:unitLookupValue="$emit('unitLookupValue', unitLookupValue )"
+		:error="error ? {message: $i18n(error.message), type: error.type} : null"
+		:numberInputValue="numberInputValue"
+		:numberInputPlaceholder="$i18n('query-builder-quantity-value-number-input-placeholder')"
+		:unitLookupValue="unitLookupValue"
+		:unitLookupPlaceholder="$i18n('query-builder-quantity-value-unit-lookup-input-placeholder')"
+		:unitLookupLabel="$i18n('query-builder-quantity-value-unit-lookup-label')"
+		:unitLookupMenuItems="searchResults"
+		:unitLookupSearchInput.sync="search"
+		:disabled="disabled"
+		v-on:scroll="handleScroll"
+	>
+		<template
+			v-slot:no-results
+		>{{ noMatchFoundMessage }}</template>
+		<template v-if="tooltip" v-slot:suffix>
+			<InfoTooltip
+				position="top-end"
+				:message="tooltip"
+			/></template>
+	</QuantityInput>
+</template>
+
+<script lang="ts">
+
+import { QuantityInput } from '@wmde/wikit-vue-components';
+import Vue from 'vue';
+import { MenuItem } from '@wmde/wikit-vue-components/dist/components/MenuItem';
+import SearchOptions from '@/data-access/SearchOptions';
+import SearchResult from '@/data-access/SearchResult';
+import InfoTooltip from '@/components/InfoTooltip.vue';
+import debounce from 'lodash/debounce';
+
+const NUMBER_OF_SEARCH_RESULTS = 12;
+
+export default Vue.extend( {
+	name: 'QuantityValueInput',
+	data() {
+		return {
+			search: '',
+			searchResults: [] as MenuItem[],
+			topItemIndex: 1,
+			numberInputValue: this.numberValue === null ? null : this.numberValue.toString(),
+			unitLookupValue: this.unitValue,
+			debouncedUpdateMenuItems: null as Function | null,
+		};
+	},
+	methods: {
+		async handleScroll( event: number ): Promise<void> {
+			if ( this.topItemIndex <= event ) {
+				this.topItemIndex += NUMBER_OF_SEARCH_RESULTS;
+
+				const searchOptions: SearchOptions = {
+					search: this.search,
+					offset: this.topItemIndex,
+					limit: NUMBER_OF_SEARCH_RESULTS,
+				};
+
+				this.searchResults = this.searchResults.concat(
+					await this.searchEntities( searchOptions ),
+				);
+			}
+		},
+		async searchEntities( searchOptions: SearchOptions ): Promise<SearchResult[]> {
+			const searchResults = await this.searchForItems( searchOptions );
+			return searchResults.map(
+				( item: MenuItem & SearchResult ) => {
+					item.tag = item.tag && this.$i18n( item.tag );
+					return item;
+				},
+			);
+		},
+		searchForItems( options: SearchOptions ): Promise<SearchResult[]> {
+			return this.$store.dispatch( 'searchItemValues', options );
+		},
+		updateMenuItems( searchOptions: SearchOptions ): void {
+			if ( this.debouncedUpdateMenuItems === null ) {
+				this.debouncedUpdateMenuItems = debounce(
+					async ( debouncedSearchOptions: SearchOptions ) => {
+						this.searchResults = await this.searchForItems( debouncedSearchOptions );
+					},
+					150,
+				);
+			}
+			this.debouncedUpdateMenuItems( searchOptions );
+		},
+	},
+	watch: {
+		disabled( isDisabled: boolean ): void {
+			if ( isDisabled ) {
+				this.search = '';
+			}
+		},
+		search( newSearchString: string ): void {
+			this.topItemIndex = 0;
+			if ( !newSearchString ) {
+				this.searchResults = [];
+				return;
+			}
+			const searchOptions: SearchOptions = {
+				search: newSearchString,
+				limit: NUMBER_OF_SEARCH_RESULTS,
+			};
+			this.updateMenuItems( searchOptions );
+		},
+	},
+	props: {
+		numberValue: {
+			type: Number,
+			default: 0,
+		},
+		unitValue: {
+			type: Object,
+			default: null,
+		},
+		error: {
+			type: Object,
+			default: null,
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		noMatchFoundMessage: {
+			type: String,
+			required: true,
+		},
+		tooltip: {
+			type: String,
+			default: '',
+		},
+	},
+	components: {
+		QuantityInput,
+		InfoTooltip,
+	},
+} );
+</script>
---
+++ tests/unit/components/QuantityValueInput.spec.ts
@@ -0,0 +1,158 @@
+import QuantityValueInput from '@/components/QuantityValueInput.vue';
+import { QuantityInput } from '@wmde/wikit-vue-components';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Vue from 'vue';
+import i18n from 'vue-banana-i18n';
+import SearchOptions from '@/data-access/SearchOptions';
+
+jest.mock( 'lodash/debounce', () => jest.fn( ( fn ) => fn ) );
+
+const localVue = createLocalVue();
+const messages = {};
+localVue.use( Vuex );
+
+Vue.use( i18n, {
+	locale: 'en',
+	messages,
+	wikilinks: true,
+} );
+
+const defaultProps = {
+	noMatchFoundMessage: 'some error copy',
+	searchForMenuItems: jest.fn(),
+};
+
+describe( 'QuantityValueInput.vue', () => {
+	it( 'bubbles numberInputValue event from the QuantityInput up', () => {
+
+		const numberValue = 10;
+		const stringNumberValue = numberValue.toString();
+		const wrapper = shallowMount( QuantityValueInput, {
+			propsData: {
+				...defaultProps,
+				numberValue,
+			} } );
+
+		wrapper.findComponent( QuantityInput ).vm.$emit( 'update:numberInputValue', stringNumberValue );
+
+		expect( wrapper.emitted( 'numberInputValue' )![ 0 ][ 0 ] ).toBe( stringNumberValue );
+	} );
+
+	it( 'bubbles unitLookupValue event from the QuantityInput up', () => {
+
+		const unitValue = { label: 'Dogs', value: 'doggos' };
+
+		const wrapper = shallowMount( QuantityValueInput, {
+			propsData: {
+				...defaultProps,
+				numberValue: 5,
+				unitValue,
+			} } );
+
+		wrapper.findComponent( QuantityInput ).vm.$emit( 'update:unitLookupValue', unitValue );
+
+		expect( wrapper.emitted( 'unitLookupValue' )![ 0 ][ 0 ] ).toBe( unitValue );
+	} );
+
+	it( 'pass numberValue prop down to QuantityInput', () => {
+		const numberValue = 10;
+
+		const wrapper = shallowMount( QuantityValueInput, {
+			propsData: {
+				...defaultProps,
+				numberValue,
+			},
+		} );
+
+		expect( wrapper.findComponent( QuantityInput ).props( 'numberInputValue' ) ).toBe( numberValue.toString() );
+
+	} );
+
+	it( 'does not error on numberValue field = null', () => {
+		const numberValue = null;
+
+		const wrapper = shallowMount( QuantityValueInput, {
+			propsData: {
+				...defaultProps,
+				numberValue,
+			},
+		} );
+
+		expect( wrapper.findComponent( QuantityInput ).props( 'numberInputValue' ) ).toBe( numberValue );
+
+	} );
+
+	it( 'pass unitValue prop down to QuantityInput', () => {
+		const unitValue = { label: 'Stars', value: 'lil stars' };
+
+		const wrapper = shallowMount( QuantityValueInput, {
+			propsData: {
+				...defaultProps,
+				numberValue: 10,
+				unitValue,
+			},
+		} );
+
+		expect( wrapper.findComponent( QuantityInput ).props( 'unitLookupValue' ) ).toBe( unitValue );
+	} );
+
+	it( 'passes error prop down to QuantityInput', () => {
+		const error = {
+			type: 'error',
+			message: 'some description',
+		};
+
+		const wrapper = shallowMount( QuantityValueInput, {
+			propsData: {
+				...defaultProps,
+				error,
+				numberValue: 10,
+			},
+		} );
+
+		expect( wrapper.findComponent( QuantityInput ).props( 'error' ) ).toStrictEqual( error );
+	} );
+
+	it( 'unit input: unitLookupSearchInput prop for unit item changes on update search string', async () => {
+		const wrapper = shallowMount( QuantityValueInput, {
+			localVue,
+			propsData: {
+				...defaultProps,
+				numberValue: 10,
+			},
+		} );
+
+		const searchOptions: SearchOptions = { search: 'meters', limit: 12 };
+
+		await wrapper.findComponent( QuantityInput ).vm.$emit( 'update:unitLookupSearchInput', searchOptions.search );
+
+		expect( wrapper.findComponent( QuantityInput ).props( 'unitLookupSearchInput' ) ).toBe( searchOptions.search );
+
+	} );
+
+	it( 'unit input: clears the search results on the search string being emptied', async () => {
+		const wrapper = shallowMount( QuantityValueInput, {
+			localVue,
+			propsData: {
+				...defaultProps,
+				numberValue: 10,
+			},
+			data() {
+				return {
+					searchResults: [
+						{ label: 'abc', description: 'def', id: 'P123' },
+						{ label: 'ghi', description: 'def', id: 'P1234' },
+						{ label: 'jkl', description: 'def', id: 'P12345' },
+					],
+					search: 'def',
+				};
+			},
+		} );
+
+		await wrapper.findComponent( QuantityInput ).vm.$emit( 'update:unitLookupSearchInput', '' );
+		expect( wrapper.findComponent( QuantityInput ).props( 'unitLookupSearchInput' ) ).toBe( '' );
+		expect( wrapper.findComponent( QuantityInput ).props( 'unitLookupMenuItems' ) ).toStrictEqual( [] );
+	} );
+
+} );
