c881e8d215cf382144f50d69e5bf8f4fd45bd238
Amir Sarabadani <Ladsgroup@gmail.com>
2021-07-29 07:04:38 +0200

    Update toolforge cron to use the build repo
    It's currently using master branch as that's the only one with actual
    assets at the moment but should change to production branch later.
    
    Bug: T276366
    Change-Id: Ie7b5f127cdf81b8dbd201d14f6c8da30bdcbc120

--- build/updater.sh
+++ build/updater.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-cd /data/project/query-builder-test/query-builder
+cd /data/project/query-builder-test/query-builder-deploy
+git checkout master
 git reset --hard origin/master
 git pull
-npm install npm@6.14.8
-./node_modules/.bin/npm install
-export NODE_ENV=production
-./node_modules/.bin/npm run build
 rm -rf public_html/*
-cp -R dist/* ../public_html/
+cp -R . ../public_html/
