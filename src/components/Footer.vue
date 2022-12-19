40e8cdc0ec37e59f7c5e254340dd28b049bcad5b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-12-19 16:24:16 +0100

    Hide underlines for all links in footer
    Including the ones generated by messages, which don’t already have their
    underlines hidden like Wikit links have. We put this in a mixin so that
    we can later reuse it for the “Show query in the Query Service” link.
    
    Also, add a :focus-visible outline for Chrome; this is just copied from
    Wikidata Mismatch Finder. (IMHO the outline is actually quite hard to
    see, but if we want to improve it, we should probably do it everywhere.)
    
    Bug: T324356
    Change-Id: I2ca1910c175eef9bf07892117d8fd462294cb1d6

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -115,10 +115,14 @@ export default Vue.extend( {
 </script>
 
 <style lang="scss">
+@use '../styles/links' as *;
+
 $tinyViewportWidth: 38em;
 $largeViewportWidth: 90em;
 
 .querybuilder-footer {
+	@include links-without-underline;
+
 	padding-block: $dimension-layout-small;
 	padding-inline: $dimension-layout-small;
 	margin-inline: 0;
