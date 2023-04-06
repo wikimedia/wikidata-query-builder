5f881b71fb49fe42ff3689c8d090461a62bb6eab
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2023-04-06 16:06:27 +0200

    Add browser tests for keyboard navigation of Language Selector
    Bug: T329488
    Change-Id: Iab02824072322c359911b46e3b3dd53431b686eb

--- cypress/e2e/language-selector.cy.js
+++ cypress/e2e/language-selector.cy.js
@@ -35,4 +35,30 @@ describe( 'Language selector component', () => {
 		cy.get( 'body' ).click( 0, 0 );
 		cy.get( '.querybuilder__language-selector' ).should( 'not.be.visible' );
 	} );
+
+	describe( 'Keyboard navigation', () => {
+		it( 'Language options are highlighted sequentially when arrowdown is pressed', () => {
+			cy.visit( '/' );
+			cy.get( '.querybuilder__languageSelector .wikit-Button' )
+				.click();
+			cy.get( '.languageSelector__input' ).type( '{downArrow}' );
+			cy.get( '.languageSelector__options-menu__languages-list__item' )
+				.eq( 0 ).should( 'have.class', 'highlight' );
+			cy.get( '.languageSelector__input' ).type( '{downArrow}' );
+			cy.get( '.languageSelector__options-menu__languages-list__item' )
+				.eq( 1 ).should( 'have.class', 'highlight' );
+		} );
+		it( 'Highlighted language loops from first to last if arrowup pressed', () => {
+			cy.visit( '/' );
+			cy.get( '.querybuilder__languageSelector .wikit-Button' )
+				.click();
+			cy.get( '.languageSelector__input' ).type( '{downArrow}' );
+			cy.get( '.languageSelector__options-menu__languages-list__item' ).its( 'length' )
+				.then( ( length ) => {
+					cy.get( '.languageSelector__input' ).type( '{upArrow}' );
+					cy.get( '.languageSelector__options-menu__languages-list__item' )
+						.eq( length - 1 ).should( 'have.class', 'highlight' );
+				} );
+		} );
+	} );
 } );
